deed22b0becdc2027f780fd7c55d1fecee7a059c
fisk810 <marc571m@edu.easj.dk>
2022-02-21 11:05:02 +0100

    Initial commit

---
+++ src/com/company/Hej.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Hej {
+    public void test(){
+        System.out.println("hej");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Hej lol = new Hej();
+    lol.test();
+    }
+}
